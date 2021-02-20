23a3235c923973f3ed6e1a3877a40e5644adfe9f
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-02-20 16:06:16 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# js-questions-for-clearing-basics
-Created with CodeSandbox
+## JavaScript Question for basic understanding
+
+These are few questions for bsic JavaScript questions.
+
+The questions are with answers (my approach).
---
+++ src/OperatorsBranchingLoops.js
@@ -0,0 +1,127 @@
+// Easy Easy Easy
+// Easy Easy Easy
+// Easy Easy Easy
+// Not marked by me, marked by industry.
+
+// Question1: Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+function addThem(a, b, c, d, e) {
+  return a + b + c + d + e;
+}
+// console.log(addThem(5, 13, 7, 21, 48));
+
+// Question2: Write a program to take a number input from user and determine whether the number is odd or even.
+function oddOrEven(a) {
+  // console.log(a % 2);
+  if (a % 2 === 0) {
+    return console.log("even");
+  }
+  return console.log("odd");
+}
+// oddOrEven(7);
+// oddOrEven(8);
+
+// Question3: Write a program to find the maximum and minimum out of two given numbers. The numbers are num1=129 and num2=251.
+// Answer1
+function findMax(a, b) {
+  if (a > b === true) {
+    return console.log(`${a} is greater than ${b}`);
+  }
+  return console.log(`${b} is greater than ${a}`);
+}
+// findMax(129, 251);
+// findMax(251, 129);
+
+// Answer2
+function findMax2(a, b) {
+  const v1 = a - b;
+  const v2 = b - b;
+  if (v1 > v2) {
+    return console.log(`${a} is greater than ${b}`);
+  }
+  return console.log(`${b} is greater than ${a}`);
+}
+// findMax2(129, 251);
+// findMax2(251, 129);
+
+// Question4:Write a program to find the maximum out of three given numbers. The numbers are num1=8, num2=23 and num3=17.
+function findMaxInThree(a, b, c) {
+  if (a > b) {
+    if (a > c) {
+      return console.log(`${a} is greater in all.`);
+    }
+    return console.log(`${c} is greater in all.`);
+  } else if (b > c) {
+    return console.log(`${b} is greater in all.`);
+  }
+  return console.log(`${c} is greater in all.`);
+}
+// findMaxInThree(8, 23, 17);
+// findMaxInThree(30, 20, 23);
+// findMaxInThree(8, 100, 17);
+
+// Question5: Write a program to find the minimum out of three given numbers. The numbers are num1=35, num2=29 and num3=46.
+function findMinInThree(a, b, c) {
+  if (a < b) {
+    if (a < c) {
+      return console.log(`${a} is smaller in all.`);
+    }
+    return console.log(`${c} is smaller in all.`);
+  } else if (b < c) {
+    return console.log(`${b} is smaller in all.`);
+  }
+  return console.log(`${c} is smaller in all.`);
+}
+// findMinInThree(35, 29, 56);
+// findMinInThree(56, 35, 29);
+// findMinInThree(29, 56, 35);
+
+// Question6: Write program to take a month as an input from the user and find out whether the month has 31 days or not.:
+// const months = [
+//   { January: 31 },
+//   { February: 28 },
+//   { March: 31 },
+//   { April: 30 },
+//   { May: 31 },
+//   { June: 30 },
+//   { July: 30 },
+//   { August: 31 },
+//   { September: 30 },
+//   { October: 31 },
+//   { November: 30 },
+//   { December: 31 }
+// ];
+// I tried it doing like this but it wasn't taking, I don't get what's wrong.
+
+const monthDays = [31, 28, 31, 30, 31, 30, 30, 31, 30, 31, 30, 31];
+const [
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+] = monthDays;
+
+function checkDaysInMonth(month) {
+  console.log(month);
+  if (month === 31) {
+    return console.log(`Yes the month has 31 days.`);
+  }
+  return console.log(`No the month doesn't has 31 days.`);
+}
+// checkDaysInMonth(January);
+// checkDaysInMonth(February);
+// checkDaysInMonth(June);
+
+// Intermediate Intermediate Intermediate
+// Intermediate Intermediate Intermediate
+// Intermediate Intermediate Intermediate
+// Not marked by me, marked by industry.
+
+// Question1: Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
