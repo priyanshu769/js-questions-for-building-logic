8e2ea320c6956738c07c3a113866557c1294e947
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-02-21 00:21:23 +0530

    Intermediate question 1

--- src/OperatorsBranchingLoops.js
+++ src/OperatorsBranchingLoops.js
@@ -125,3 +125,38 @@ function checkDaysInMonth(month) {
 // Not marked by me, marked by industry.
 
 // Question1: Fizzbuzz - Write a program to return an array from 1 to 100. But for every multiple of 3, replace the number with "Fizz", for every multiple of 5, replace the number with "Buzz" and for every multiples of 3 & 5, replace with "FizzBuzz".
+function fizzBuzz() {
+  const array = [];
+  for (let i = 1; i <= 100; i++) {
+    array.push(i);
+  }
+  // console.log(array);
+  for (let j = 0; j <= array.length; j++) {
+    if (array[j] % 3 === 0 && array[j] % 5 === 0) {
+      array[j] = "fizzbuzz";
+      // console.log(array[j]);
+      // console.log("fizz");
+      // array.splice(array[j], "fizz");
+    } else if (array[j] % 3 === 0) {
+      array[j] = "fizz";
+      // console.log(array[j]);
+      // console.log("buzz");
+      // array.splice(array[j], "buzz");
+    } else if (array[j] % 5 === 0) {
+      array[j] = "buzz";
+      // console.log(array[j]);
+      // console.log("fizzbuzz");
+      // array.splice(array[j], "fizzbuzz");
+    }
+  }
+  console.log(array);
+}
+fizzBuzz();
+// I did it somehow, first of all I did third statement in every if else but it wasn't working then I checked by logging it that if the for loop is working or not. Then I saw it was working and then I did that array[j]= "something" and boom, I got it.
+
+// Question2: Print the following star pattern :-
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
