ae3fee795af8f34f40a30ca120b13f9cc11f7331
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-02-28 16:37:10 +0530

    Functions Question 5

--- src/Functions.js
+++ src/Functions.js
@@ -26,7 +26,12 @@ const numberOfWords = (a) => {
 
 // Question4: Given n numbers, your function should return the minimum of them all. The number of parameters won't be accepted from user.
 
-const minNum = (...array) => console.log(Math.min(...array));
-// minNum(9,8,3,6,3,7,5);
+const minNum = (...array) => Math.min(...array);
+// console.log(minNum(9,8,3,6,3,7,5));
 
-// Question5:
+// Question5: Given n numbers, your function should return the maximum of them all. The number of parameters won't be accepted from user.
+const maxNum = (...array) => Math.max(...array);
+
+// console.log(maxNum(9, 8, 3, 6, 3, 7, 5));
+
+// Question6: Given three angles of a triange, your function should return if it is a scalen, isosceles, equilateral triangle or not a triangle at all.
