036f197a5d9a8430c57fc7555382644c4de37eb9
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-03-02 18:10:19 +0530

    Functions - (Length of an array question)

---
+++ src/Functions/Medium/LengthOfAnArray.js
@@ -0,0 +1,6 @@
+// Question1: Given an array, your function should return the length of the array.
+
+const lengthOfArray = (array) => {
+  return array.length;
+};
+// console.log(lengthOfArray([1, 2, 3, 4, 5, 6, 78, 9]));
