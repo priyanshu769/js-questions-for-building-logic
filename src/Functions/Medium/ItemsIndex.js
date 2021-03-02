a787885821d0b9889c7152de4590bc6dc271a93a
Priyanshu <74850965+priyanshu769@users.noreply.github.com>
2021-03-02 18:15:17 +0530

    Functions - (Items Index question)

---
+++ src/Functions/Medium/ItemsIndex.js
@@ -0,0 +1,5 @@
+// Question2: Given an array and an item, your function should return the index at which the item is present.
+
+const itemsIndex = (array, item) => array.indexOf(item);
+
+// console.log(itemsIndex([1, 23, 5, 6], 5));
